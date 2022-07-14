f3b6408679884b0b21ab7573c5fd8e533c22a264
arif-pandu <mapanduprayoga@gmail.com>
2022-07-14 11:42:04 +0700

    Add : New branch for testing coordinates

--- lib/helper/coordinates.dart
+++ lib/helper/coordinates.dart
@@ -3,16 +3,26 @@ import 'package:flutter_snake_game/provider/game_provider.dart';
 import 'package:provider/provider.dart';
 
 class Coordinates {
-  // double pixel(BuildContext context) => Provider.of<GameController>(context).snakePartSize;
-  List listCoordinates = [
-    ...List.generate(
-      20,
-      (index) => [
+  double partSize(BuildContext context) {
+    var game = Provider.of<GameController>(context, listen: false);
+
+    return game.snakePartSize;
+  }
+
+  // BIKIN LIST COORDINATES BIAR GAMPANG
+
+  List listCoordinates(BuildContext context) => [
         ...List.generate(
           20,
-          (indexToo) => indexToo,
+          (indexRow) => [
+            ...List.generate(
+              20,
+              (indexColumn) => [
+                partSize(context) * indexRow,
+                partSize(context) * indexColumn,
+              ],
+            ),
+          ],
         ),
-      ],
-    ),
-  ];
+      ];
 }
